50d61363046d050efc0e4571e6529cacd0c7260d
SergiiSiedash <sergiisiedash@gmail.com>
2024-05-18 16:47:26 +0400

    "May 18th Week 10 Module 16 Project task_3 Completed"

---
+++ conftest.py
@@ -0,0 +1,24 @@
+import pytest
+
+class User:
+
+    def __init__(self) -> None:
+        self.name = None
+        self.second_name = None
+
+    def create(self):
+        self.name = 'Sergii'
+        self.second_name = 'Butenko'
+
+    def remove(self):
+        self.name = ''
+        self.second_name = ''
+
+@pytest.fixture
+def user():
+     user = User()
+     user.create()
+
+     yield user # Все, що знаходиться до слова yield буде виконано до тесту 
+
+     user.remove
