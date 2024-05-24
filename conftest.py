818c40b17f2fbf1c7b30181e4537f532d5c370bd
SergiiSiedash <sergiisiedash@gmail.com>
2024-05-24 14:43:11 +0400

    project task 4 required part

--- conftest.py
+++ conftest.py
@@ -1,4 +1,6 @@
 import pytest
+from modules.api.clients.github import GitHub
+
 
 class User:
 
@@ -7,18 +9,25 @@ class User:
         self.second_name = None
 
     def create(self):
-        self.name = 'Sergii'
-        self.second_name = 'Butenko'
+        self.name = "Sergii"
+        self.second_name = "Butenko"
 
     def remove(self):
-        self.name = ''
-        self.second_name = ''
+        self.name = ""
+        self.second_name = ""
+
 
 @pytest.fixture
 def user():
-     user = User()
-     user.create()
+    user = User()
+    user.create()
 
-     yield user # Все, що знаходиться до слова yield буде виконано до тесту 
+    yield user  # Все, що знаходиться до слова yield буде виконано до тесту
 
-     user.remove
+    user.remove
+
+
+@pytest.fixture
+def github_api():
+    api = GitHub()
+    yield api
